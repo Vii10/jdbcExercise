c21959c40c268286797bd6e3232f7d80e893636f
The John! <jdworks1998@gmail.com>
2022-10-24 13:09:27 -0300

    UPDATE - How to update a DB

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -14,72 +10,37 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		//Precisaremos criar um SDF para atribuir uma data de nascimento
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Connection conn = null;
-		PreparedStatement ps = null;
+		//Var's de conexão
+		Connection con = null;
+		PreparedStatement pt = null;
 		
 		try {
-			conn = DB.getConnection();
+			//Conectando
+			con = DB.getConnection();
 			
-			//Não há necessidade de adicionar o id na string, pois ele é auto-incrementável pelo SQL
-			ps = conn.prepareStatement(
-				"INSERT INTO seller" //Adiciona na tabela seller (Escrito em linguagem SQL)
-				+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)" //Seleciona os campos que serão inseridos os dados
-				+ "VALUES " //Valores que serão inseridos
-				+ "(?, ?, ?, ?, ?)", //A interrogação é um placeHolder que "Segura" os campos para serem alterados depois
-				Statement.RETURN_GENERATED_KEYS); //Realizando sobrecarga para retornar as chaves criadas através de um enumerated
+			//Instrução de atualização dos dados
+			pt = con.prepareStatement(
+					"UPDATE seller " //Acessando a tabela para ser atualizada
+					+ "SET BaseSalary = BaseSalary + ? " //Atualizar salário com base no que já tinha + um valor digitado
+					+ "WHERE " //Restrição para definir somente quando a condição da linha a baixo for verdadeira
+					+ "(DepartmentId = ?)"); //Condição para a alteração do salário
 			
-			//Adicionando valores
-			ps.setString(1, "Carlito Franguito"); //Acessa o primeiro "?" e atribui à ele um valor. Usa-se cada set de acordo com o tipo de dado
-			ps.setString(2, "carlito@gmail.com");
-			ps.setDate(3, new java.sql.Date(sdf.parse("31/02/1996").getTime()));
-			/* O setDate precisará importar o sql.Date ao invés do util.Date, pois estamos trabalhando com SQL - JDBC e não um programa 100% JAVA
-			 * A função .getTime pega a data que escrevemos e a converte para um valor "entendível" para o sql.Date
-			 * A função .parse realiza a conversão da String da data em uma data verdadeira*/
-			ps.setDouble(4, 3000.);
-			ps.setInt(5, 3);
+			//Atribuindo valores
+			pt.setDouble(1, 200.00);
+			pt.setInt(2, 2);
 			
-			/*
-			//Testando inserção de departamentos novos
-			ps = conn.prepareStatement(
-					"insert into department (Names) ('D1'), ('D2')"
-					, Statement.RETURN_GENERATED_KEYS);
-			*/
+			//Executar atualização e atribuir a uma varíavel
+			int rows = pt.executeUpdate();
 			
-			//Para realizar a alteração dos dados
-			int rowsAffected = ps.executeUpdate(); //Essa função retorna um número inteiro que exibe o valor das linhas alteradas
-			
-			//Para fins de Debug e estudo, realiza-se o print da varíavel anterior
-			System.out.println("DONE! Rows affecteds: " + rowsAffected);
-			
-			//Tratamento especial para retornar as chaves
-			if(rowsAffected > 0) {
-				ResultSet rs = ps.getGeneratedKeys(); //A função .getGeneratedKeys retorna um objeto do tipo ResultSet, por isso a criação dele
-				
-				//Percorrendo o ResultSet
-				while(rs.next()) { //A função .next foi explicada no commit anterior
-					int id = rs.getInt(1); //Pega a primeira coluna da tabela inserida no objeto ResultSet
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
+			//Mostrar linhas afetadas
+			System.out.println("Done! Rows affected: " + rows);
 		}
-		catch (SQLException e) {
-			e.printStackTrace(); //A função .printStackTrace exibe uma lista de passos que acarretaram o erro
-		}
-		catch (ParseException e) {
+		catch(SQLException e) {
 			e.printStackTrace();
 		}
 		finally {
-			//Fechamento manual dos recursos abertos
-			DB.closeStatement(ps);
+			DB.closeStatement(pt);
 			DB.closeConnection();
-			//Por questões lógicas, sempre fecharemos a conexão por último
 		}
 	}
-
 }
