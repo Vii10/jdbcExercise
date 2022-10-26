88efeb25519598fa17bbd21b97bd32a867cbb72f
The John! <jdworks1998@gmail.com>
2022-10-25 23:41:06 -0300

    UPDATE - Transactions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 public class Program {
 
@@ -13,31 +13,49 @@ public class Program {
 		
 		//Var's de conexão
 		Connection con = null;
-		PreparedStatement pt = null;
+		Statement st = null;
 		
 		try {
 			//Conectando
 			con = DB.getConnection();
+			st = con.createStatement(); //Criando Statement
 			
-			//Instrução de atualização dos dados
-			pt = con.prepareStatement(
-					"DELETE FROM department " //Deletando dados da tabela de departamentos
-					+"WHERE "	//Condicional de função = Só deleta se:
-					+"Id = ?"); //Valor que torna a condição acima verdadeira
+			//Criando lógica de transação usando a função de autocommit()
+			con.setAutoCommit(false);
 			
-			pt.setInt(1, 5); //Atribuindo valores
+			//Variáveis de colunas atribuidas à execução da atualização da var st
+			int rows = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			//Executar atualização e atribuir a uma varíavel
-			int rows = pt.executeUpdate();
+			/*//Teste de erro fake durante a atualização das informações
+			int x = 1;
+			if(x < 2) {
+				throw new SQLException("Fake error");
+			}
+			*/
 			
-			//Mostrar linhas afetadas
-			System.out.println("Done! Rows affected: " + rows);
+			int rowsTwo = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			//Lê-se: "Atualizar o vendedor; Mude o valor base para tanto quando o id do departamento for tal"
+			
+			//A função .commit realiza a confirmação do fim da transação. Assim os dados só são passados após a excecução dela
+			con.commit();
+			
+			//Auto-explicativo
+			System.out.println("Rows 1: " + rows);
+			System.out.println("Rows 2: " + rowsTwo);
 		}
+		
+			
 		catch(SQLException e) {
-			throw new DbIntegrityException(e.getMessage()); //Lançando exception personalizada
+			//A função .rollback retorna o estado original dos dados antes das alterações. Porém, ela dá uma SQLException
+			try { //Try de tratamento da incapacidade de realizar a transação
+				con.rollback();
+				throw new DbException("Transaction failed! Caused by " + e.getMessage());
+			} catch (SQLException e1) { //Catch de tratamento no rollback! Acontece quando se torna impossível voltar
+				throw new DbException("Rollback error " + e1.getMessage());
+			}
 		}
 		finally {
-			DB.closeStatement(pt);
+			DB.closeStatement(st);
 			DB.closeConnection();
 		}
 	}
